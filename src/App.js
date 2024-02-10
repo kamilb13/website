5a40998bef1fc1a25787da14dfd487a015b9eac2
kamilb13 <kamil.brzoza13@gmail.com>
2024-02-10 11:16:13 +0100

    naprawa 1.5

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MyForm from './MyForm';
 import NextPage from './NextPage';
-import Balloon from './Balloon';
 import EndPage from './EndPage';
 
 const App = () => {
   return (
     <>
     <h1>hejka</h1>
-    <Router>
-      <Routes>
-        <Route path="/" element={<MyForm />} />
-        <Route path="/inna-strona" element={<NextPage />} />
-        <Route path="/end-page" element={<EndPage />} />
-      </Routes>
-    </Router>
+
     </>
   );
 };
