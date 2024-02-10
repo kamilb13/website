407f90ca9c6fc6ff537a028a5699a447962abda6
kamilb13 <kamil.brzoza13@gmail.com>
2024-02-10 11:28:22 +0100

    naprawa 1.6

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import EndPage from './EndPage';
 
 const App = () => {
   return (
-    <Router basename="/website">
-      <Routes>
-        <Route path="/" element={<MyForm />} />
-        <Route path="/inna-strona" element={<NextPage />} />
-        <Route path="/end-page" element={<EndPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MyForm />} />
+      <Route path="/inna-strona" element={<NextPage />} />
+      <Route path="/end-page" element={<EndPage />} />
+    </Routes>
   );
 };
 
