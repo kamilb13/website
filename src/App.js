f62fa852eaa7c31a7bab23dfb11d8f0e051a44c5
kamilb13 <kamil.brzoza13@gmail.com>
2024-02-10 11:20:56 +0100

    naprawa 1.6

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import EndPage from './EndPage';
 
 const App = () => {
   return (
-    <>
-    <h1>hejka</h1>
-
-    </>
+    <Router basename="/website">
+      <Routes>
+        <Route path="/" element={<MyForm />} />
+        <Route path="/inna-strona" element={<NextPage />} />
+        <Route path="/end-page" element={<EndPage />} />
+      </Routes>
+    </Router>
   );
 };
 
